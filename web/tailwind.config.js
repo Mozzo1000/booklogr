d08b56d45cf99c002ae5fc1e6ac40ffd18bb5789
Andreas Backström <andreas@simplymozzo.se>
2024-03-24 19:24:55 +0100

    Add flowbite-react dependency

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,10 +3,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin"),],
 }
 
