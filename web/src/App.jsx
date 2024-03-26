5adf9fdeca8683eaeda9a4ffb4b6a192a88b256d
Andreas Backström <andreas@simplymozzo.se>
2024-03-26 19:18:58 +0100

    Add routes

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,11 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import BookDetails from "./pages/BookDetails";
+import Home from "./pages/Home";
+import SearchBar from "./components/SearchBar";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home/>} />
+        <Route path="books/:id" element={<BookDetails />} />
+        <Route path="search" element={<SearchBar />} />
+      </Route>
+    </Routes>
     </>
   )
 }
