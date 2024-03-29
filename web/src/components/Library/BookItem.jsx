b34a690569f259ec48dcbff21542fcc82472c5ad
Andreas Backström <andreas@simplymozzo.se>
2024-03-29 16:25:06 +0100

    Add full width to BookItem inner container

--- web/src/components/Library/BookItem.jsx
+++ web/src/components/Library/BookItem.jsx
@@ -6,7 +6,7 @@ function BookItem(props) {
     return (
         <div className="flex flex-col bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-md dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
             <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-24 md:rounded-none md:rounded-s-lg" src={"https://covers.openlibrary.org/b/isbn/" + props.isbn + "-L.jpg"} alt=""></img>
-            <div className="flex flex-col justify-between p-4 leading-normal">
+            <div className="flex flex-col justify-between p-4 leading-normal w-full">
                 <Link to={"/books/" + props.isbn} className="hover:underline">
                     <h5 className="mb-2 text font-bold tracking-tight text-gray-900 dark:text-white">{props.title}</h5>
                 </Link>
