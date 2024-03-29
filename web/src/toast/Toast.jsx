26ac3c8448ba251fedf94a888b6aecd6eb53208f
Andreas Backström <andreas@simplymozzo.se>
2024-03-29 19:51:26 +0100

    Add icons to toast message

--- web/src/toast/Toast.jsx
+++ web/src/toast/Toast.jsx
@@ -1,19 +1,21 @@
 import { Alert } from "flowbite-react";
 import { useToastDispatchContext } from "./Context";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { MdErrorOutline } from "react-icons/md";
 
 export default function Toast({ type, message, id }) {
     const dispatch = useToastDispatchContext();
     return (
         <>
             {type == "success" && (
-                <Alert color="success" onDismiss={() => {
+                <Alert icon={IoMdInformationCircleOutline} color="success" onDismiss={() => {
                     dispatch({ type: "DELETE_TOAST", id });
                 }}>
                     {message}
                 </Alert>
             )}
             {type == "error" && (
-                <Alert color="failure" onDismiss={() => {
+                <Alert icon={MdErrorOutline} color="failure" onDismiss={() => {
                     dispatch({ type: "DELETE_TOAST", id });
                 }}>
                     {message}
