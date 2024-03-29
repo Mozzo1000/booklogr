b1bafca67afc4e285a5899888fdbf26925869c4a
Andreas Backström <andreas@simplymozzo.se>
2024-03-29 12:19:47 +0100

    Add LibraryPane

---
+++ web/src/components/Library/LibraryPane.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react'
+import BookItem from './BookItem'
+import { Tabs } from "flowbite-react";
+import BooksService from '../../services/books.service';
+
+function LibraryPane() {
+    const [activeTab, setActiveTab] = useState(0);
+    const [books, setBooks] = useState();
+    
+    useEffect(() => {
+        let status = "Currently reading";
+        if (activeTab == 0) {
+            status = "Currently reading"
+        } else if (activeTab == 1) {
+            status = "To be read"
+         }else if (activeTab == 2) {
+            status = "Read"
+        }
+        BooksService.get(status).then(
+            response => {
+                setBooks(response.data)
+                console.log(response.data)
+            }
+        )
+    }, [activeTab])
+    
+
+    return (
+        <>
+        <Tabs onActiveTabChange={(tab) => setActiveTab(tab)} style='fullWidth'>
+        <Tabs.Item active title="Currently reading">
+            {books?.map((item) => {
+                return (
+                    <div key={item.id}>
+                    <BookItem showProgress={true} title={item.title} isbn={item.isbn} totalPages={item.total_pages} currentPage={item.current_page}/>
+                    </div>
+                )
+            })}
+        </Tabs.Item>
+        <Tabs.Item title="To be read">
+            {books?.map((item) => {
+                return (
+                    <div key={item.id}>
+                    <BookItem showProgress={false} title={item.title} isbn={item.isbn} totalPages={item.total_pages} currentPage={item.current_page}/>
+                    </div>
+                )
+            })}
+        </Tabs.Item>
+        <Tabs.Item title="Read">
+            {books?.map((item) => {
+                return (
+                    <div key={item.id}>
+                    <BookItem showProgress={false} title={item.title} isbn={item.isbn}/>
+                    </div>
+                )
+            })}
+        </Tabs.Item>
+        </Tabs>
+        {books?.length <= 0 &&
+            <p>No books found</p>
+        }
+        </>
+    )
+}
+
+export default LibraryPane
--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import LibraryPane from '../components/Library/LibraryPane'
 
 function Home() {
   return (
-    <div>
+    <div className="container mx-auto">
         <p>Home</p>
+        
+        <LibraryPane />
     </div>
   )
 }
