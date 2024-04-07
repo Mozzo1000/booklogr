b89484af18fbe7b1783318757e8233d8eb159b2f
Andreas Backström <andreas@simplymozzo.se>
2024-04-07 15:17:23 +0200

    Fix height of BookItem

--- web/src/components/Library/BookItem.jsx
+++ web/src/components/Library/BookItem.jsx
@@ -6,7 +6,7 @@ import ChangeBookLibraryButton from '../ChangeBookLibraryButton';
 
 function BookItem(props) {
     return (
-        <div className="flex flex-col bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-md dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div className="min-h-full flex flex-col bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-md dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
             <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-24 md:rounded-none md:rounded-s-lg" src={"https://covers.openlibrary.org/b/isbn/" + props.isbn + "-L.jpg"} alt=""></img>
             <div className="flex flex-col justify-between p-4 leading-normal w-full">
                 <Link to={"/books/" + props.isbn} className="hover:underline">
