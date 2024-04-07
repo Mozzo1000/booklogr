a0e13f73bc59d131c1ec15a8289aee4dcf833f2a
Andreas Backström <andreas@simplymozzo.se>
2024-04-07 15:50:39 +0200

    Improve search skeleton loaders

--- web/src/components/SearchBar.jsx
+++ web/src/components/SearchBar.jsx
@@ -3,13 +3,15 @@ import axios from "axios";
 import { debounce } from 'lodash';
 import { Spinner, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function SearchBar() {
     const [searchTerm, setSearchTerm] = useState('');
     const [suggestions, setSuggestions] = useState([{id: 0, name: ""}]);
     const [loading, setLoading] = useState(false);
     const [showList, setShowList] = useState(false);
-
+    const loadingPlaceholder = [0,1,2,3,4,5]
   
     const fetchSuggestions = (searchTerm) => {
       if (searchTerm) {
@@ -56,22 +58,25 @@ function SearchBar() {
             <TextInput id="search" type="text" placeholder="Search for a book" onChange={(e) => (debouncedChangeHandler(e), setSearchTerm(e.target.value))} value={searchTerm} />
             <div className={`${showList? "block": "hidden"} absolute z-10 bg-white pt-10 max-w-md  overflow-y-auto	max-h-96 min-w-28 min-h-28`}>
                 {loading ? (
-                     <>
-                     <div className="grid grid-cols-2 grid-rows-1 pt-2 animate-pulse">
-                         <div className="row-span-2">
-                         <div class=" bg-gray-300 rounded">
-                            <svg class="text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
-                            </svg>
-                        </div>
-                         </div>
-                         <div className="col-start-2">
-                            <div class="h-2.5 bg-gray-200 rounded-full  w-48 mb-4"></div>
-                            <div class="h-2 bg-gray-200 rounded-full  mb-2.5"></div>
-                         </div>
-                     </div>
-                     <hr/>
-                     </>
+                     loadingPlaceholder.map(function() {
+                        return (
+                            <div>
+                                <div className="grid grid-cols-2 grid-rows-1 pt-2">
+                                    <div className="row-span-2">
+                                        <div class="h-24 w-24 bg-gray-300 rounded">
+                                            <svg class="h-24 w-24 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className="col-start-2">
+                                        <Skeleton count={2} width={850} />
+                                    </div>
+                                </div>
+                                <hr/>
+                            </div>
+                        )
+                    })
                 ): (
                     suggestions?.map(function(data) {
                         return (
@@ -82,10 +87,9 @@ function SearchBar() {
                                 </div>
                                 <div className="col-start-2">
                                     <Link to={"/books/" + data.isbn} onClick={() => showList(false)}>
-                                        <p key={data.id}>{data.name}</p>
+                                        <p>{data.name}</p>
                                         {data.isbn}
                                     </Link>
-
                                 </div>
                             </div>
                             <hr/>
