19d5ba40d86f107c7709eeb549c1f0b5b0aed27b
Andreas Backström <andreas@simplymozzo.se>
2024-07-01 20:29:55 +0200

    Fix link navigation issue

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { FaBook } from "react-icons/fa6";
 import { FaCircleUser } from "react-icons/fa6";
 import { TbLayoutSidebarLeftCollapseFilled } from "react-icons/tb";
 import { FaPlug } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 function NavigationMenu() {
     const [sidebarState, setSidebarState] = useState(true);
@@ -15,7 +16,7 @@ function NavigationMenu() {
     return (
         <>
         <Sidebar collapsed={sidebarState}>
-          <Sidebar.Logo href="/" img="/icon.svg" className="mr-3 h-6 sm:h-8" alt="Logo">
+          <Sidebar.Logo as={Link} href="/" img="/icon.svg" className="mr-3 h-6 sm:h-8" alt="Logo">
             <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">minimal reading</span>
           </Sidebar.Logo>
             <Sidebar.Items>
@@ -27,10 +28,13 @@ function NavigationMenu() {
                     )}
                   </Sidebar.ItemGroup>
                   <Sidebar.ItemGroup>
-                    <Sidebar.Item href="/" icon={FaBook}><span className="font-semibold">My Library</span></Sidebar.Item>
-                    <Sidebar.Item href="profile" icon={FaCircleUser }><span>Profile</span></Sidebar.Item>
-                    <Sidebar.Item href="#" icon={FaPlug }><span>Integrations</span></Sidebar.Item>
-
+                    <Link to="/">
+                      <Sidebar.Item icon={FaBook}><span className="font-semibold">My Library</span></Sidebar.Item>
+                    </Link>
+                    <Link to="/profile">
+                      <Sidebar.Item icon={FaCircleUser }><span>Profile</span></Sidebar.Item>
+                    </Link>
+                    <Sidebar.Item icon={FaPlug }><span>Integrations</span></Sidebar.Item>
                 </Sidebar.ItemGroup>
                 <Sidebar.ItemGroup>
                   <Sidebar.Item icon={sidebarState ? TbLayoutSidebarRightCollapseFilled : TbLayoutSidebarLeftCollapseFilled } onClick={() => setSidebarState(!sidebarState)}>
