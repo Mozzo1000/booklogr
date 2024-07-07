2e9811326be7ca015e9d3e9bdb4a81f35cdf6783
Andreas Backström <mozzo242@gmail.com>
2024-07-07 19:32:33 +0200

    Change default background color

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -10,13 +10,19 @@ import { FaRightFromBracket } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import AuthService from '../services/auth.service';
 
+const customTheme = {
+  root: {
+    inner: "h-full overflow-y-auto overflow-x-hidden rounded bg-[#FDFCF7] py-4 px-3 dark:bg-gray-800 border-r",
+  }
+};
+
 function NavigationMenu() {
     const [sidebarState, setSidebarState] = useState(true);
     const [openSearchModal, setOpenSearchModal] = useState(false);
 
     return (
         <>
-        <Sidebar collapsed={sidebarState} className="min-h-screen">
+        <Sidebar collapsed={sidebarState} theme={customTheme} className="min-h-screen">
           <Sidebar.Logo as={Link} href="/" img="/icon.svg" className="mr-3 h-6 sm:h-8" alt="Logo">
             <span className="self-center whitespace-nowrap text-xl dark:text-white font-[800]">BookLogr</span>
           </Sidebar.Logo>
