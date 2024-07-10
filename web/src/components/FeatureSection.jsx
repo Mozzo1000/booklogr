8860992241d1aec72a5f88162f9d6510274b8e37
Andreas Backström <mozzo242@gmail.com>
2024-07-10 17:09:53 +0200

    Add shadows to images

--- web/src/components/FeatureSection.jsx
+++ web/src/components/FeatureSection.jsx
@@ -9,7 +9,7 @@ function FeatureSection() {
       <div class="grid grid-cols-1 md:grid-cols-2 max-w-screen-xl py-8 mx-auto gap-12 lg:grid-cols-12">
           {/* First feature */}
           <div class="lg:mt-0 lg:col-span-5 order-1 p-4">
-              <img src="/feature_section_01.png" alt="Library picture"/>
+              <img src="/feature_section_01.png" alt="Library picture" className="shadow-2xl"/>
           </div>
           <div class="place-self-center lg:col-span-7 order-2 p-4 md:p-0">
               <h2 class="text-white max-w-2xl mb-4 text-4xl font-semibold tracking-tight leading-none md:text-4xl xl:text-5xl">Build your virtual library</h2>
@@ -23,12 +23,12 @@ function FeatureSection() {
               <p class="max-w-2xl mb-6 text-white lg:mb-8 md:text-md lg:text-lg">Let your friends see what kind of exciting book you are currently reading and have read in the past</p>
           </div>
           <div class="lg:mt-0 lg:col-span-5 order-3 lg:order-4 p-4">
-              <img src="/feature_section_02.png" alt="Library picture"/>
+              <img src="/feature_section_02.png" alt="Library picture" className="shadow-2xl"/>
           </div>
 
           {/* Third feature */}
           <div class="lg:mt-0 lg:col-span-5 order-5 p-4">
-              <img src="/feature_section_03.png" alt="Library picture"/>
+              <img src="/feature_section_03.png" alt="Library picture" className="shadow-2xl"/>
           </div>
           <div class="place-self-center lg:col-span-7 order-6 p-4 md:p-0">
               <h2 class="text-white max-w-2xl mb-4 text-4xl font-semibold tracking-tight leading-none md:text-4xl xl:text-5xl">Look up your favorite book with ease</h2>
