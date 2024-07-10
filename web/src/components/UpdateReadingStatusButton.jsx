f5d8d23df2cd869688acc4b17c7a6445da8cc5d3
Andreas Backström <mozzo242@gmail.com>
2024-07-10 17:23:03 +0200

    Add congratulations screen when finishing a book

--- web/src/components/UpdateReadingStatusButton.jsx
+++ web/src/components/UpdateReadingStatusButton.jsx
@@ -1,11 +1,16 @@
 import React, { useState }  from 'react'
-import { Button, TextInput, Modal, Label } from 'flowbite-react'
+import { Button, TextInput, Modal } from 'flowbite-react'
 import { RiBookOpenLine } from "react-icons/ri";
 import BooksService from '../services/books.service';
 import useToast from '../toast/useToast';
+import Confetti from 'react-confetti'
+import { RiMastodonFill } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 
 function UpdateReadingStatusButton(props) {
     const [openModal, setOpenModal] = useState(false);
+    const [openFinishModal, setOpenFinishModal] = useState(false);
+
     const [updatedProgress, setUpdatedProgress] = useState();
     const toast = useToast(4000);
 
@@ -24,7 +29,7 @@ function UpdateReadingStatusButton(props) {
             response => {
                 toast("success", response.data.message);
                 setOpenModal(false);
-                props.onSucess()
+                setOpenFinishModal(true);
             }
         )
     }
@@ -48,6 +53,26 @@ function UpdateReadingStatusButton(props) {
                 <Button onClick={() => updateProgress()}>Update</Button>
                 <Button color="gray" onClick={() => setOpenModal(false)}> Cancel </Button>
                 <Button color="gray" onClick={() => setFinished()}>Set as finished</Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={openFinishModal} onClose={() => (setOpenFinishModal(false), props.onSucess())} popup>
+                <Modal.Header />
+                <Modal.Body>
+                    <Confetti width={"640"} height={386} recycle={false} />
+                    <div className="flex flex-col items-center justify-center text-center gap-2">
+                        <img src={"/medal.svg"} width={140} height={140}/>
+                        <div className="format lg:format-lg">
+                            <h2>Congratulations!</h2>
+                            <p>On finishing reading <strong>{props.title}</strong></p>
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer className="flex justify-center gap-4">
+                    <Button as={Link} to={"https://mastodonshare.com/?text=I just finished reading " + props.title  + " 📖&url=https://booklogr.app/books/" + props.id} target='_blank'>
+                        <RiMastodonFill className="mr-2 h-5 w-5" />
+                        Share on Mastodon
+                    </Button>
                 </Modal.Footer>
             </Modal>
         </>
