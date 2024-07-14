3bc8506b9b880a85bf2d47f102e903a76bb2ff07
Andreas Backström <mozzo242@gmail.com>
2024-07-14 13:50:41 +0200

    Change name to booklogr

--- api/config.py
+++ api/config.py
@@ -4,4 +4,4 @@ class Config:
     CSRF_ENABLED = True
     SECRET_KEY = os.environ.get("AUTH_SECRET_KEY", "this-really-needs-to-be-changed")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://admin:password@localhost/minimal-reading")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://admin:password@localhost/booklogr")
