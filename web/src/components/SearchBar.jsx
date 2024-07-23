861aaf8fc28f4f646555c5d495f4cad805257e29
Andreas Backström <mozzo242@gmail.com>
2024-07-23 18:57:38 +0200

    Add cover fallback and better loading indication to search results

--- web/src/components/SearchBar.jsx
+++ web/src/components/SearchBar.jsx
@@ -7,6 +7,7 @@ import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { RiSearch2Line } from "react-icons/ri";
 import ESCIcon from "./ESCIcon";
+import { Img } from 'react-image'
 
 function SearchBar(props) {
     const [searchTerm, setSearchTerm] = useState('');
@@ -68,13 +69,9 @@ function SearchBar(props) {
                      loadingPlaceholder.map(function() {
                         return (
                             <div>
-                                <div className="grid grid-cols-2 grid-rows-1 pt-2">
+                                <div className="grid grid-cols-2 grid-rows-1 pt-2 pb-2">
                                     <div className="row-span-2">
-                                        <div class="h-24 w-24 bg-gray-300 rounded">
-                                            <svg class="h-24 w-24 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                                                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
-                                            </svg>
-                                        </div>
+                                            <Skeleton count={1} height={100} width={"60%"} />
                                     </div>
                                     <div className="col-start-2">
                                         <Skeleton count={2} width={850} />
@@ -88,9 +85,12 @@ function SearchBar(props) {
                     suggestions?.map(function(data) {
                         return (
                             <div key={data.id}>
-                            <div className="grid grid-cols-2 grid-rows-1 pt-2">
+                            <div className="grid grid-cols-2 grid-rows-1 pt-2 pb-2">
                                 <div className="row-span-2">
-                                    <img className="object-contain h-24 w-24" src={"https://covers.openlibrary.org/b/isbn/" + data.isbn +"-S.jpg"} />
+                                    <Img className="object-contain h-24 w-24" src={"https://covers.openlibrary.org/b/isbn/" + data.isbn + "-S.jpg?default=false"} 
+                                        loader={<Skeleton count={1} width={100} height={"100%"} borderRadius={0} inline={true}/>}
+                                        unloader={<img className="object-contain h-24 w-24" src="/fallback-cover.svg"/>}
+                                    />
                                 </div>
                                 <div className="col-start-2">
                                     <Link to={"/books/" + data.isbn} onClick={() => showList(false)}>
