5f595cfceddb937ebe7b84a80e676c55c2eeff27
Andreas Backström <mozzo242@gmail.com>
2024-07-23 19:43:39 +0200

    Add icons to reading status buttons

--- web/src/components/ActionsBookLibraryButton.jsx
+++ web/src/components/ActionsBookLibraryButton.jsx
@@ -7,6 +7,9 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import { RiErrorWarningLine } from "react-icons/ri";
 import NotesView from './NotesView';
 import { RiStickyNoteLine } from "react-icons/ri";
+import { RiBook2Line } from "react-icons/ri";
+import { RiBookOpenLine } from "react-icons/ri";
+import { RiBookmarkLine } from "react-icons/ri";
 
 function ActionsBookLibraryButton(props) {
     const [status, setStatus] = useState();
@@ -63,15 +66,15 @@ function ActionsBookLibraryButton(props) {
         <>
         <Dropdown label="" dismissOnClick={false} renderTrigger={() => <span className="hover:cursor-pointer"><FaEllipsisVertical /></span>}>
             <Dropdown.Header>
-                <span className="block text-sm font-medium">Reading status</span>
-                <Dropdown.Divider/>
+                <span className="block text-sm font-bold">Reading status</span>
             </Dropdown.Header>
-            <Dropdown.Item onClick={() => (clickDropItem("Currently reading"))}>Currently reading</Dropdown.Item>
-            <Dropdown.Item onClick={() => (clickDropItem("To be read"))}>To be read</Dropdown.Item>
-            <Dropdown.Item onClick={() => (clickDropItem("Read"))}>Read</Dropdown.Item>
+            <Dropdown.Item onClick={() => (clickDropItem("Currently reading"))}><RiBookOpenLine size={18} className="mr-1"/>Currently reading</Dropdown.Item>
+            <Dropdown.Item onClick={() => (clickDropItem("To be read"))}><RiBookmarkLine size={18} className="mr-1"/>To be read</Dropdown.Item>
+            <Dropdown.Item onClick={() => (clickDropItem("Read"))}><RiBook2Line size={18} className="mr-1"/>Read</Dropdown.Item>
             <Dropdown.Divider />
-            <Dropdown.Item onClick={() => setOpenNotesModal(true)}><RiStickyNoteLine className="mr-1"/>Notes</Dropdown.Item>
-            <Dropdown.Item onClick={() => setRemovalConfModal(true)}><RiDeleteBin6Line className="mr-1" />Remove</Dropdown.Item>
+
+            <Dropdown.Item onClick={() => setOpenNotesModal(true)}><RiStickyNoteLine size={18} className="mr-1"/>Notes</Dropdown.Item>
+            <Dropdown.Item onClick={() => setRemovalConfModal(true)}><RiDeleteBin6Line size={18} className="mr-1" />Remove</Dropdown.Item>
         </Dropdown>
 
         {/* REMOVE BOOK CONFIRMATION DIALOG */}
