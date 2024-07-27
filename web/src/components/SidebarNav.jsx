39b4aa7f269f81c847defbbdb7c7f1a4f829eeca
Andreas Backström <mozzo242@gmail.com>
2024-07-27 09:58:42 +0200

    Add settings button to mobile navigation bar

--- web/src/components/SidebarNav.jsx
+++ web/src/components/SidebarNav.jsx
@@ -65,7 +65,7 @@ function SidebarNav() {
 
         {/* Mobile bottom navigation bar */}
         <div className="md:hidden fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-            <div className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium">
+            <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
                 <Link to="/library" className="inline-flex flex-col pt-2">
                   <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                       <RiBook2Line className={`w-5 h-5 mb-2 group-hover:text-blue-600 ${location.pathname == "/library" ? "text-blue-600" : "text-gray-500"}`}/>
@@ -82,6 +82,12 @@ function SidebarNav() {
                   <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                       <RiUser3Line className={`w-5 h-5 mb-2 group-hover:text-blue-600 ${location.pathname == "/profile" ? "text-blue-600" : "text-gray-500"}`}/>
                       <span className={`text-sm dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500 ${location.pathname == "/profile" ? "text-blue-600" : "text-gray-500"}`}>Profile</span>
+                  </button>
+                </Link>
+                <Link to="/settings" className="inline-flex flex-col pt-2">
+                  <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                      <RiSettings4Line className={`w-5 h-5 mb-2 group-hover:text-blue-600 ${location.pathname == "/settings" ? "text-blue-600" : "text-gray-500"}`}/>
+                      <span className={`text-sm dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500 ${location.pathname == "/settings" ? "text-blue-600" : "text-gray-500"}`}>Settings</span>
                   </button>
                 </Link>
             </div>
