b331ad89deb74b94eb13007e14ba751f8d63d469
Andreas Backström <mozzo242@gmail.com>
2024-09-06 20:11:19 +0200

    Add docs to profiles route #7

--- api/routes/profiles.py
+++ api/routes/profiles.py
@@ -7,6 +7,22 @@ profiles_endpoint = Blueprint('profiles', __name__)
 
 @profiles_endpoint.route("/v1/profiles/<display_name>", methods=["GET"])
 def get_profile(display_name):
+    """
+        Get profile by name
+        ---
+        tags:
+            - Profiles
+        parameters:
+            - name: display_name
+              in: path
+              type: string
+              required: true
+        responses:
+          200:
+            description: Information about the profile.
+          404:
+            description: Profile not found.
+    """
     profile_schema = ProfileSchema()
     
     profile = Profile.query.join(Profile.books).join(Books.notes).filter(Profile.display_name==display_name, Profile.visibility=="public", Notes.visibility=="public").first()
@@ -22,6 +38,19 @@ def get_profile(display_name):
 @profiles_endpoint.route("/v1/profiles", methods=["GET"])
 @jwt_required()
 def get_profile_by_logged_in_id():
+    """
+        Get profile of the logged in user
+        ---
+        tags:
+            - Profiles
+        security:
+            - bearerAuth: []         
+        responses:
+          200:
+            description: Information about the profile of the logged in user.
+          404:
+            description: No profile found.
+    """
     claim_id = get_jwt()["id"]
     profile_schema = ProfileSchema()
     
@@ -39,6 +68,30 @@ def get_profile_by_logged_in_id():
 @profiles_endpoint.route("/v1/profiles", methods=["POST"])
 @jwt_required()
 def create_profile():
+    """
+        Create profile for logged in user
+        ---
+        tags:
+            - Profiles
+        parameters:
+            - name: display_name
+              in: body
+              type: string
+              required: true
+            - name: visibility
+              in: body
+              type: string
+              default: hidden
+              required: false
+        security:
+            - bearerAuth: []         
+        responses:
+          200:
+            description: Profile created.
+
+          409:
+            description: Profile already exists.
+    """
     claim_id = get_jwt()["id"]
 
     profile = Profile.query.filter(Profile.owner_id==claim_id).first()
@@ -61,6 +114,28 @@ def create_profile():
 @profiles_endpoint.route("/v1/profiles", methods=["PATCH"])
 @jwt_required()
 def edit_profile():
+    """
+        Edit profile
+        ---
+        tags:
+            - Profiles
+        parameters:
+            - name: display_name
+              in: body
+              type: string
+              required: false
+            - name: visibility
+              in: body
+              type: string
+              required: false
+        security:
+            - bearerAuth: []         
+        responses:
+          200:
+            description: Profile updated.
+          404:
+            description: Profile not found.
+    """
     claim_id = get_jwt()["id"]
 
     profile = Profile.query.filter(Profile.owner_id==claim_id).first()
