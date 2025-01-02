0f8e3d5b441138a3c5095360bf2aedd4a2c59c28
Andreas Backström <mozzo242@gmail.com>
2025-01-02 20:31:12 +0100

    Change layout of account page

--- web/src/components/AccountTab.jsx
+++ web/src/components/AccountTab.jsx
@@ -21,38 +21,39 @@ function AccountTab() {
             
             <hr />
 
-            <div className="flex flex-col gap-4 max-w-lg">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 <div className="format lg:format-lg">
-                    <h3 >Login email</h3>
+                    <h4>Email</h4>
                 </div>
                 <div>
-                    <Label htmlFor="email1" value="Email" />
                     <TextInput id="email1" type="email" placeholder="name@flowbite.com" icon={RiMailLine} />
                 </div>
             </div>
 
             <hr />
 
-            <div className="flex flex-col gap-4 max-w-lg">
+            <div className="grid grid-cols-1 md:grid-cols-3">
                 <div className="format lg:format-lg">
-                    <h3 >Change password</h3>
+                    <h4 >Change password</h4>
                 </div>
-                <div>
-                    <Label htmlFor="password1" value="Old password" />
-                    <TextInput id="password1" type="password" />
-                </div>
+                <div className="flex flex-col gap-4">
+                    <div>
+                        <Label htmlFor="password1" value="Old password" />
+                        <TextInput id="password1" type="password" />
+                    </div>
 
-                <div >
-                    <Label htmlFor="password2" value="New password" />
-                    <TextInput id="password2" type="password" />
-                </div>
+                    <div >
+                        <Label htmlFor="password2" value="New password" />
+                        <TextInput id="password2" type="password" />
+                    </div>
 
-                <div>
-                    <Label htmlFor="password3" value="Confirm new password" />
-                    <TextInput id="password3" type="password" />
-                </div>
+                    <div>
+                        <Label htmlFor="password3" value="Confirm new password" />
+                        <TextInput id="password3" type="password" />
+                    </div>
 
-                <Button>Change password</Button>
+                    <Button>Change password</Button>
+                    </div>
             </div>
                
         </div>
