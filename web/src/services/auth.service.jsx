bdf59d84c1bff4ba356ca8d34e38168a10bfd420
Andreas Backström <mozzo242@gmail.com>
2025-06-21 15:56:34 +0200

    Change over to builtin auth endpoints

--- web/src/services/auth.service.jsx
+++ web/src/services/auth.service.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import globalRouter from "../GlobalRouter";
 
-const API_URL = import.meta.env.VITE_AUTH_API_URL;
+const API_URL = import.meta.env.VITE_API_ENDPOINT;
 
 
 axios.interceptors.response.use((response) => {
