1b254a89725ff5c8bf86218876397a4fbdb16211
Andreas Backström <mozzo242@gmail.com>
2025-06-21 17:07:58 +0200

    Update react dependencies, remove react-helmet-async

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -5,16 +5,13 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { ToastProvider } from './toast/Context.jsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ToastProvider>
         <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-          <HelmetProvider>
-            <App />
-          </HelmetProvider>
+          <App />
         </GoogleOAuthProvider>
       </ToastProvider>
     </BrowserRouter>
