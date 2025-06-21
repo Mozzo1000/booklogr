043e2e31854976073b261a21ddb9de281444402a
Andreas Backström <mozzo242@gmail.com>
2025-06-21 15:55:14 +0200

    Add auth_default_user check to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,11 @@
 
 echo "Running database migration.."
 python -m flask db upgrade
+
+if [ -n "${AUTH_DEFAULT_USER+1}" ]; then
+  echo "\$AUTH_DEFAULT_USER is set"
+  python -m flask user create "$AUTH_DEFAULT_USER" "$AUTH_DEFAULT_USER" user --password "$AUTH_DEFAULT_PASSWORD" 
+fi
+
 echo "Starting gunicorn.."
 exec gunicorn -w 4 -b :5000 api.app:app
