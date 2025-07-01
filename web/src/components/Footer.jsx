ea856a99bfa0abd46aafc5ec676aa18a522c96c4
Andreas Backström <mozzo242@gmail.com>
2025-07-01 14:58:40 +0200

    Change Footer and Navbar to apply dark variant

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { DarkThemeToggle } from "flowbite-react";
 
 function Footer() {
   return (
-    <FFooter container className="bg-transparent sticky top-[100vh] z-10 border-none shadow-none ">
+    <FFooter container className="bg-transparent dark:bg-transparent sticky top-[100vh] z-10 border-none shadow-none ">
       <div className="w-full sm:flex sm:items-center sm:justify-between">
         <span></span>
         <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
