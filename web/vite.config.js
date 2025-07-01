58556208e0d9d361b855c958b7fa9ad944d6261e
Andreas Backström <mozzo242@gmail.com>
2025-07-01 21:36:00 +0200

    Fix flowbite not initializing correctly after update

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), flowbiteReact()],
 });
