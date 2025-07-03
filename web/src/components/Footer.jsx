a070e01e1011aaf1ba0c784f33d0c51d93d0fcaa
Andreas Backström <mozzo242@gmail.com>
2025-07-03 21:08:38 +0200

    Hide footer on small screens

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { DarkThemeToggle } from "flowbite-react";
 
 function Footer() {
   return (
-    <FFooter container className="bg-transparent dark:bg-transparent sticky top-[100vh] z-10 border-none shadow-none ">
+    <FFooter container className="hidden md:block bg-transparent dark:bg-transparent sticky top-[100vh] z-10 border-none shadow-none ">
       <div className="w-full sm:flex sm:items-center sm:justify-between">
         <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center items-center">
             <p className="format dark:format-invert text-xs">v{import.meta.env.VITE_APP_VERSION}</p>
