033e279e9b612e19df161c04303676fba7d11a33
Andreas Backström <mozzo242@gmail.com>
2025-07-10 18:27:00 +0200

    Change meta description

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
   return (
     <Layout
       title={``}
-      description="Description will go into a meta tag in <head />">
+      description="Self-hosted reading tracker">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
