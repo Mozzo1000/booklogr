2ca4ec5c14d2fe1d0bc74702c1cab73c466d55c5
Andreas Backström <mozzo242@gmail.com>
2025-07-19 17:46:30 +0200

    Add fullSize option to language switcher

--- web/src/components/LanguageSwitcher.jsx
+++ web/src/components/LanguageSwitcher.jsx
@@ -1,7 +1,7 @@
 import { Dropdown, DropdownItem } from 'flowbite-react'; 
 import { useTranslation } from 'react-i18next';
 
-function LanguageSwitcher() {
+function LanguageSwitcher({fullSize}) {
     const { i18n } = useTranslation();
     const languages = [
         {code: "en", label: "English", flag: "🇬🇧"},
@@ -9,13 +9,14 @@ function LanguageSwitcher() {
     ]
 
     const currentLang = languages.find(lang => lang.code === i18n.language)?.flag || '🌐';
+    const fullLang = languages.find(lang => lang.code === i18n.language)?.label;
 
     const changeLanguage = (lng) => {
         i18n.changeLanguage(lng);
     }
 
     return (
-        <Dropdown label={currentLang} inline>
+        <Dropdown label={fullSize ? currentLang + " " +fullLang : currentLang} inline={!fullSize} color={'alternative'} className="max-h-48 overflow-y-auto">
             {languages.map(({ code, label, flag }) => (
                 <DropdownItem key={code} onClick={() => changeLanguage(code)}>
                     {flag} {label}
