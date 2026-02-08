3e95580c06a3a0bf02f373ed43b800695cfab7bf
Andreas Backström <mozzo242@gmail.com>
2026-02-08 11:00:28 +0100

    Make title, author and total pages field editable

--- web/src/components/ActionsBookLibraryButton.jsx
+++ web/src/components/ActionsBookLibraryButton.jsx
@@ -64,7 +64,7 @@ function ActionsBookLibraryButton(props) {
         
         <NotesView id={props.id} open={openNotesModal} setOpen={setOpenNotesModal} allowEditing={props.allowNoteEditing}/>
         <RemoveBookModal id={props.id} open={openRemoveModal} close={setOpenRemoveModal} onSuccess={props.onSuccess}/>
-        <EditBookModal id={props.id} totalPages={props.totalPages} open={openEditBookModal} close={setOpenEditBookModal} onSuccess={props.onSuccess} />
+        <EditBookModal id={props.id} totalPages={props.totalPages} open={openEditBookModal} close={setOpenEditBookModal} onSuccess={props.onSuccess} author={props.author} title={props.title} isbn={props.isbn}/>
         </>
     )
 }
--- web/src/components/Library/EditBookModal.jsx
+++ web/src/components/Library/EditBookModal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Modal, ModalHeader, ModalBody, ModalFooter, TextInput, Button, Popover, Label, HR} from 'flowbite-react';
+import { Modal, ModalHeader, ModalBody, ModalFooter, TextInput, Button, Popover, Label, HR, Tooltip} from 'flowbite-react';
 import useToast from '../../toast/useToast';
 import BooksService from '../../services/books.service';
 import { RiQuestionLine } from "react-icons/ri";
@@ -12,12 +12,15 @@ import { useThemeMode } from 'flowbite-react';
 function EditBookModal(props) {
     const [saveButtonDisabled, setSaveButtonDisabled] = useState(false);
     const [totalPages, setTotalPages] = useState(props.totalPages);
+    const [author, setAuthor] = useState(props.author);
+    const [title, setTitle] = useState(props.title);
+    const [isbn, setISBN] = useState(props.isbn);
     const toast = useToast(4000);
     const theme = useThemeMode();    
     const { t } = useTranslation();
     
     const handleEditBook = () => {
-        BooksService.edit(props.id, {total_pages: parseInt(totalPages)}).then(
+        BooksService.edit(props.id, {total_pages: parseInt(totalPages), title: title, author: author}).then(
             response => {
                 toast("success", response.data.message);
                 props.close(false);
@@ -38,42 +41,55 @@ function EditBookModal(props) {
 
     return (
         <>
-            <Modal show={props.open} onClose={() => props.close(false)}>
+            <Modal dismissible show={props.open} onClose={() => props.close(false)}>
                 <ModalHeader className="border-gray-200">{t("actions.edit_book")}</ModalHeader>
                 <ModalBody>
-                    <div className="flex flex-col gap-12">
+                    <div className="flex flex-col gap-8">
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
                             <div className="format lg:format-lg dark:format-invert">
-                                <h4>{t("sort.title")}</h4>
+                                <p>{t("sort.title")}</p>
                             </div>
                             <div className="col-span-2">
-                                <TextInput type="text" />
+                                <TextInput type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
                             </div>
                         </div>
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
                             <div className="format lg:format-lg dark:format-invert">
-                                <h4>{t("sort.author")}</h4>
+                                <p>{t("sort.author")}</p>
                             </div>
                             <div className="col-span-2">
-                                <TextInput type="text" />
+                                <TextInput type="text" value={author} onChange={(e) => setAuthor(e.target.value)}/>
                             </div>
                         </div>
+
+
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
                             <div className="format lg:format-lg dark:format-invert">
-                                <h4>{"Cover"}</h4>
+                                <p>ISBN</p>
+                            </div>
+                            <div className="col-span-2">
+                                <TextInput disabled type="text" value={isbn} onChange={(e) => setISBN(e.target.value)}/>
+                            </div>
+                        </div>
+
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
+                            <div className="format lg:format-lg dark:format-invert">
+                                <p>{"Cover"}</p>
                             </div>
                             <div>
-                                <Img className="shadow-2xl object-fit rounded" src={"https://covers.openlibrary.org/b/id/" + 0 + "-M.jpg?default=false"} 
+                                <Img className="shadow-2xl object-fit rounded" src={"https://covers.openlibrary.org/b/isbn/" + isbn + "-M.jpg?default=false"} 
                                     loader={<Skeleton count={1} width={200} height={200} borderRadius={0} inline={true}/>}
                                     unloader={theme.mode == "dark" && <img src="/fallback-cover-light.svg"/> || theme.mode == "light" && <img src="/fallback-cover.svg"/>}
                                 />
-                                <Button disabled>Replace cover</Button>
+                                <Tooltip content="Currently not implemented">
+                                    <Button disabled>Replace cover</Button>
+                                </Tooltip>
                             </div>
                         </div>
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                             <div className="format lg:format-lg dark:format-invert">
-                                <h4>{t("book.total_pages")}</h4>
-                                <p className="text-xs">{t("help.number_of_pages_information")}</p>
+                                <p>{t("book.total_pages")}
+                                <div className="text-xs">{t("help.number_of_pages_information")}</div></p>
                             </div>
                             <div className="col-span-2 ">
                                 <TextInput type="number" value={totalPages} onChange={(e) => setTotalPages(e.target.value)} />
