abed05d1c9b5ffa2d547efd32f4ac9893ebada69
BjarnePW <Bjarne_Elberskirch@gmx.de>
2026-02-16 21:40:07 +0100

    Added german translation

--- web/src/i18n.jsx
+++ web/src/i18n.jsx
@@ -5,26 +5,30 @@ import en from "./locales/en/en.json";
 import sv from "./locales/sv/sv.json";
 import ar from "./locales/ar/ar.json";
 import cn from "./locales/zh-CN/zh-CN.json";
+import de from "./locales/de/de.json";
 
 const resources = {
-	en: {
-		translation: en,
-	},
-	sv: {
-		translation: sv,
-	},
-	ar: {
-		translation: ar,
-	},
-	cn: {
-		translation: cn,
-	},
+  en: {
+    translation: en,
+  },
+  ar: {
+    translation: ar,
+  },
+  cn: {
+    translation: cn,
+  },
+  de: {
+    translation: de,
+  },
+  sv: {
+    translation: sv,
+  },
 };
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
-	resources,
-	fallbackLng: "en",
-	debug: true,
+  resources,
+  fallbackLng: "en",
+  debug: true,
 });
 
 export default i18n;
