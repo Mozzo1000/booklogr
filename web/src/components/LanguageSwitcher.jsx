97633bd33f53903c387f62c23d4d890f67ad145b
bjarnepw <77669988+bjarnepw@users.noreply.github.com>
2026-03-23 18:35:35 +0100

    Added german translation (#78)

--- web/src/components/LanguageSwitcher.jsx
+++ web/src/components/LanguageSwitcher.jsx
@@ -1,31 +1,38 @@
-import { Dropdown, DropdownItem } from 'flowbite-react'; 
-import { useTranslation } from 'react-i18next';
+import { Dropdown, DropdownItem } from "flowbite-react";
+import { useTranslation } from "react-i18next";
 
-function LanguageSwitcher({fullSize}) {
-    const { i18n } = useTranslation();
-    const languages = [
-        {code: "en", label: "English", flag: "🇬🇧"},
-        {code: "sv", label: "Svenska", flag: "🇸🇪"},
-        {code: "ar", label: "عربي", flag: "🇸🇦"},
-        {code: "cn", label: "中文", flag: "🇨🇳"}
-    ]
+function LanguageSwitcher({ fullSize }) {
+  const { i18n } = useTranslation();
+  const languages = [
+    { code: "en", label: "English", flag: "🇬🇧" },
+    { code: "sv", label: "Svenska", flag: "🇸🇪" },
+    { code: "ar", label: "عربي", flag: "🇸🇦" },
+    { code: "cn", label: "中文", flag: "🇨🇳" },
+    { code: "de", label: "Deutsch", flag: "🇩🇪" },
+  ];
 
-    const currentLang = languages.find(lang => lang.code === i18n.language)?.flag || '🌐';
-    const fullLang = languages.find(lang => lang.code === i18n.language)?.label;
+  const currentLang =
+    languages.find((lang) => lang.code === i18n.language)?.flag || "🌐";
+  const fullLang = languages.find((lang) => lang.code === i18n.language)?.label;
 
-    const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
-    }
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
 
-    return (
-        <Dropdown label={fullSize ? currentLang + " " +fullLang : currentLang} inline={!fullSize} color={'alternative'} className="max-h-48 overflow-y-auto">
-            {languages.map(({ code, label, flag }) => (
-                <DropdownItem key={code} onClick={() => changeLanguage(code)}>
-                    {flag} {label}
-                </DropdownItem>
-            ))}
-        </Dropdown>
-    )
+  return (
+    <Dropdown
+      label={fullSize ? currentLang + " " + fullLang : currentLang}
+      inline={!fullSize}
+      color={"alternative"}
+      className="max-h-48 overflow-y-auto"
+    >
+      {languages.map(({ code, label, flag }) => (
+        <DropdownItem key={code} onClick={() => changeLanguage(code)}>
+          {flag} {label}
+        </DropdownItem>
+      ))}
+    </Dropdown>
+  );
 }
 
-export default LanguageSwitcher
+export default LanguageSwitcher;
