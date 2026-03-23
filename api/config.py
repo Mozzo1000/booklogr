ad3e140db5e87de3ccc8c8fba768b8480a08ef63
Andreas Backström <mozzo242@gmail.com>
2026-03-23 20:25:49 +0100

    Remove default auth_secret_key and alert if not set / prev default set.

--- api/config.py
+++ api/config.py
@@ -1,9 +1,10 @@
 import os
 from datetime import timedelta
+from .utils import alert
 
 class Config:
     CSRF_ENABLED = True
-    SECRET_KEY = os.environ.get("AUTH_SECRET_KEY", "this-really-needs-to-be-changed")
+    SECRET_KEY = os.environ.get("AUTH_SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://admin:password@localhost/booklogr")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=5)
@@ -35,3 +36,7 @@ class Config:
         },
         "specs_route": "/docs"
     }
+    if SECRET_KEY == "this-really-needs-to-be-changed":
+            alert("USE OF DEPRECATED KEY DETECTED!\nCHANGE THE ENV VAR AUTH_SECRET_KEY TO A RANDOM, UNIQUE STRING")
+    elif not SECRET_KEY:
+        alert("NO SECRET KEY SET!\nYOU WILL HAVE TO SET THE ENV VAR AUTH_SECRET_KEY TO A RANDOM STRING BEFORE BOOKLOGR WILL START.")
