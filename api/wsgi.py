f9b0bf5c229f2c4c3ccd059baed0d207681cad7f
Andreas Backström <mozzo242@gmail.com>
2026-03-30 15:27:11 +0200

    Add wsgi entrypoint

---
+++ api/wsgi.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from waitress import serve
+from api.app import app, db
+from flask_migrate import upgrade
+from api.models import UserSettings
+from api.auth.models import User, Verification
+import secrets
+import string
+
+def setup_desktop_environment():
+    with app.app_context():
+        if getattr(sys, 'frozen', False):
+            print("Desktop Mode: Ensuring database is ready...")
+            try:
+                upgrade()
+            except Exception:
+                db.create_all()
+        
+        if app.config.get("SINGLE_USER_MODE") == "True":
+            existing_user = User.query.filter(User.id==1).first()
+            if existing_user:
+                return "Default profile already exists. Skipping bootstrap."
+            
+            rand_pass = ''.join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(32))
+
+            try:
+                new_user = User(email="single@user.com", password=User.generate_hash(rand_pass), name="Local user", role="admin")
+                new_user.save_to_db()
+
+                new_verification = Verification(user_id=new_user.id, status="verified", code=None, code_valid_until=None)
+                new_verification.save_to_db()
+
+                print(f"Successfully bootstrapped single user")
+
+            except Exception as e:
+                print(f"Bootstrap Error: {e}")
+
+if __name__ == "__main__":
+    setup_desktop_environment()
+    
+    host = "127.0.0.1"
+    port = 5000
+    
+    print(f"BookLogr API running at http://{host}:{port}")
+    serve(app, host=host, port=port, threads=4)
