fcff0dff29ededacc1e1813abd2c4e183b63da76
Andreas Backström <mozzo242@gmail.com>
2026-03-30 15:29:17 +0200

    Add sidecar

--- web/src-tauri/src/main.rs
+++ web/src-tauri/src/main.rs
@@ -1,6 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_plugin_shell::ShellExt;
+
 fn main() {
-  app_lib::run();
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            // Start the Python Sidecar
+            let sidecar_command = app.shell().sidecar("api-backend").unwrap();
+            let (mut _rx, mut _child) = sidecar_command
+                .spawn()
+                .expect("Failed to spawn sidecar");
+
+            println!("Python Sidecar started successfully.");
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
