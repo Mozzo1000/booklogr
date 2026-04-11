da4a18ce608f776835d61d3d1052dfe6b4d91ad4
Andreas Backström <mozzo242@gmail.com>
2026-04-11 10:08:09 +0200

    Change Google login button to fill screen width

--- web/src/components/GoogleLoginButton.jsx
+++ web/src/components/GoogleLoginButton.jsx
@@ -32,7 +32,7 @@ function GoogleLoginButton(props) {
     });
 
     const googleButton = (
-        <Button disabled={props.error} color="light" onClick={() => (handleLoginGoogle(), setLoading(true))}>
+        <Button disabled={props.error} color="light" onClick={() => (handleLoginGoogle(), setLoading(true))} className="w-full">
         <svg version="1.1" className="roboto-medium h-5 w-5 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlnsXlink="http://www.w3.org/1999/xlink">
             <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
             <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
@@ -71,7 +71,7 @@ function GoogleLoginButton(props) {
         ): (
             props.error ? (
                 <Popover placement='top' content={displayPopoverContent} trigger="hover">
-                    <div>
+                    <div className="w-full">
                         {googleButton}
                     </div>
                 </Popover>
