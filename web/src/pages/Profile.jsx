d0ed224a5a0777ca21c65fb21976116cb4b676c0
Andreas Backström <mozzo242@gmail.com>
2026-04-12 19:18:05 +0200

    Change gap between stats items to 4 instead of 8 #84

--- web/src/pages/Profile.jsx
+++ web/src/pages/Profile.jsx
@@ -179,7 +179,7 @@ function Profile() {
                         }                        
                         </div>
                     </div>
-                    <div className="grid grid-cols-2 md:grid-cols-4 gap-8 pt-12">
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-8 pt-12">
                         <BookStatsCard icon={<RiBook2Line className="w-8 h-8 dark:text-white"/>} number={data?.num_books_read || 0} text={t("profile.stats.read")}/>
                         <BookStatsCard icon={<RiBookOpenLine className="w-8 h-8 dark:text-white"/>} number={data?.num_books_reading || 0} text={t("profile.stats.reading")}/>
                         <BookStatsCard icon={<RiBookmarkLine className="w-8 h-8 dark:text-white"/>} number={data?.num_books_tbr || 0} text={t("profile.stats.to_be_read")}/>
