1442917bcaf2fad0ee923403dbc5d53aa6b5bb8a
Andreas Backström <mozzo242@gmail.com>
2026-05-17 13:24:56 +0200

    Add default env variables when injecting into build

--- web/inject-env.sh
+++ web/inject-env.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# Default env variables, if not set use it will use these defaults.
+export BL_ALLOW_MANUAL_LOGIN="${BL_ALLOW_MANUAL_LOGIN:-true}"
+export BL_ALLOW_REGISTRATION="${BL_ALLOW_REGISTRATION:-true}"
+export BL_GOOGLE_ID="${BL_GOOGLE_ID:-""}"
+export BL_OIDC_CLIENT_ID="${BL_OIDC_CLIENT_ID:-""}"
+
 for i in $(env | grep BL_)
 do
     key=$(echo $i | cut -d '=' -f 1)
