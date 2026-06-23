77902032b3c6b87aa53bd3bafe8bc64dc1f40036
Andreas Backström <mozzo242@gmail.com>
2026-06-23 20:34:09 +0200

    Fix missing key prop on BookSkeleton list items

--- web/src/components/BookSkeleton.jsx
+++ web/src/components/BookSkeleton.jsx
@@ -7,7 +7,7 @@ const BookSkeleton = ({ view, count = 3, showProgress = true }) => {
     return (
         <>
         {Array(count).fill(0).map((_, i) => (
-        <div className={`${view === "gallery" ? "flex-col min-h-full" : "min-w-full flex-row"} flex bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-md dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700`}>
+        <div key={i} className={`${view === "gallery" ? "flex-col min-h-full" : "min-w-full flex-row"} flex bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-md dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700`}>
                     <div className={`${view === "gallery" ? "w-full h-186" : "w-60 h-52"} object-cover  rounded-t-lg md:h-auto md:w-24 md:rounded-s-lg`}>
                         <Skeleton count={1} width={96} height={"100%"} borderRadius={0} inline={true}/>
                     </div>
