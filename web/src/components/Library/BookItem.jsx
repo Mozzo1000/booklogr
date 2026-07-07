09ac0467962e5e336b56ff0173a32ccd51f5791f
royto <royto81+github@gmail.com>
2026-07-07 22:28:26 +0200

    design: better design

--- web/src/components/Library/BookItem.jsx
+++ web/src/components/Library/BookItem.jsx
@@ -17,23 +17,23 @@ function BookItem(props) {
 
     return (
         <div className={`${props.view === "gallery" ? "flex-col min-h-full" : "min-w-full flex-row"} flex bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-md dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700`}>
-                <Img crossorigin="anonymous" className={`${props.view === "gallery" ? "w-full h-80" : "w-60 h-52"} object-cover  rounded-t-lg md:h-auto md:w-24 md:rounded-s-lg`} src={"https://covers.openlibrary.org/b/isbn/" + props.isbn + "-M.jpg?default=false"}
-                    loader={<Skeleton count={1} width={96} height={"100%"} borderRadius={0} inline={true}/>}
-                    unloader={theme.mode == "dark" && <img className="object-fit h-56 w-full rounded-t-lg md:h-auto md:w-24 md:rounded-s-lg" src="/fallback-cover-light.svg"/> || theme.mode == "light" && <img className="object-fit h-56 w-full rounded-t-lg md:h-auto md:w-24 md:rounded-s-lg" src="/fallback-cover.svg"/>}
-
-                />
-                <div className="flex flex-col p-4 leading-normal w-full overflow-hidden">
+            <Img crossorigin="anonymous" className={`${props.view === "gallery" ? "w-full h-80" : "w-60 h-52"} object-cover rounded-t-lg md:h-auto md:w-24 md:rounded-s-lg`} src={"https://covers.openlibrary.org/b/isbn/" + props.isbn + "-M.jpg?default=false"}
+                loader={<Skeleton count={1} width={96} height={"100%"} borderRadius={0} inline={true}/>}
+                unloader={theme.mode == "dark" && <img className="object-fit h-56 w-full rounded-t-lg md:h-auto md:w-24 md:rounded-s-lg" src="/fallback-cover-light.svg"/> || theme.mode == "light" && <img className="object-fit h-56 w-full rounded-t-lg md:h-auto md:w-24 md:rounded-s-lg" src="/fallback-cover.svg"/>}
+            />
+            <div className="flex flex-col gap-2 p-4 leading-normal w-full overflow-hidden">
                 <Link to={"/books/" + props.isbn} className="hover:underline dark:decoration-white">
-                    <h5 className="mb-2 text font-bold tracking-tight text-gray-900 dark:text-white">{props.title}</h5>
+                    <h5 className="text font-bold tracking-tight text-gray-900 dark:text-white">{props.title}</h5>
                 </Link>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 truncate">
+                <div className="grow">
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                     {props.author && props.onAuthorClick ? (
                         <span
                             role="button"
                             tabIndex={0}
                             onClick={() => props.onAuthorClick(props.author)}
                             onKeyDown={(e) => { if (e.key === "Enter" || e.key === " ") { e.preventDefault(); props.onAuthorClick(props.author); } }}
-                            className="cursor-pointer hover:underline focus:outline-none"
+                            className="cursor-pointer hover:underline focus:outline-none whitespace-nowrap block truncate"
                         >
                             {t("book.by_author", {author: props.author})}
                         </span>
@@ -53,27 +53,38 @@ function BookItem(props) {
                             <div className={`${props.view === "gallery" ? "grow" : ""}`}>
                                 <UpdateReadingStatusButton currentPage={props.currentPage} totalPages={props.totalPages} id={props.internalID} title={props.title} rating={props.rating} onSucess={props.onReadingStatusChanged}/>
                             </div>
-                            <div>
-                                <ActionsBookLibraryButton id={props.internalID} onSuccess={props.onReadingStatusChanged} allowNoteEditing={props.allowNoteEditing} totalPages={props.totalPages} author={props.author} title={props.title} subtitle={props.subtitle} isbn={props.isbn} description={props.description} readingStatus={props.readingStatus}/>
-                            </div>
                         </div>
                     </div>
                 }
-                <div className="flex flex-row justify-between">
-                {props.showRating &&
-                    <BookRating id={props.internalID} title={props.title} rating={props.rating} disableGiveRating={props.disableGiveRating} />
-                }
-                {props.showNotes &&
-                    (props.notes > 0 &&
-                        <NotesIcon id={props.internalID} notes={props.notes} allowEditing={props.allowNoteEditing} overrideNotes={props.overrideNotes} />
-                    )
-                }
                 </div>
+                <div className="flex flex-row justify-between content-center">
+                    <div className="flex flex-row gap-2 content-center items-center">
+                    {props.showRating &&
+                        <BookRating id={props.internalID} title={props.title} rating={props.rating} disableGiveRating={props.disableGiveRating} />
+                    }
+                    {props.showNotes &&
+                        (props.notes > 0 &&
+                            <NotesIcon id={props.internalID} notes={props.notes} allowEditing={props.allowNoteEditing} overrideNotes={props.overrideNotes} />
+                        )
+                    }
+                    </div>
+
                 {props.showOptions &&
-                    <div className="flex flex-row-reverse">
-                        <ActionsBookLibraryButton id={props.internalID} onSuccess={props.onReadingStatusChanged} allowNoteEditing={props.allowNoteEditing} totalPages={props.totalPages} author={props.author} title={props.title} subtitle={props.subtitle} isbn={props.isbn} description={props.description} readingStatus={props.readingStatus}/>
+                    <div className="justify-self-end content-center">
+                        <ActionsBookLibraryButton
+                            id={props.internalID}
+                            onSuccess={props.onReadingStatusChanged}
+                            allowNoteEditing={props.allowNoteEditing}
+                            totalPages={props.totalPages}
+                            author={props.author}
+                            title={props.title}
+                            subtitle={props.subtitle}
+                            isbn={props.isbn}
+                            description={props.description}
+                            readingStatus={props.readingStatus}/>
                     </div>
                 }
+                </div>
             </div>
         </div>
     )
