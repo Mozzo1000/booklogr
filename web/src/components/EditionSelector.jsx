f99a059dddc294e837596e730cad5139d4cd4782
julien roy <royto81@gmail.com>
2026-09-03 23:01:23 +0200

    Filter editions by valid ISBN-13 and clean up rendering logic in EditionSelector

--- web/src/components/EditionSelector.jsx
+++ web/src/components/EditionSelector.jsx
@@ -20,7 +20,9 @@ function EditionSelector({work_id, selected_isbn}) {
         if (work_id) {
             OpenlibraryService.getEditions(work_id, 100).then(
                 response => {
-                    setEditionList(response.data.entries)
+                    const editions = response.data.entries
+                                        .filter(entry => entry.isbn_13 && entry.isbn_13.length > 0);
+                    setEditionList(editions);
                 },
                 error => {
                     const resMessage =
@@ -94,12 +96,10 @@ function EditionSelector({work_id, selected_isbn}) {
             <div className="max-h-96 overflow-y-auto">
                 {filteredEntries?.map(function(data) {
                     return (
-                        data.isbn_13?.[0] && (
                             <DropdownItem key={data.isbn_13[0]} as={Link} to={"/books/" + data.isbn_13[0]}>
                                 <EditionItem data={data} selected_isbn={selected_isbn}/>
                             </DropdownItem>
                         )
-                    )
                 })}
             </div>
         </Dropdown>
@@ -111,14 +111,12 @@ function EditionSelector({work_id, selected_isbn}) {
                 <LanguageSelector />
                 {filteredEntries?.map(function(data) {
                     return (
-                        data.isbn_13?.[0] && (
                             <Link key={data.isbn_13[0]} to={"/books/" + data.isbn_13[0]} className="contents">
                                 <div className="flex w-full hover:bg-gray-100">
                                     <EditionItem data={data} selected_isbn={selected_isbn} />
                                 </div>
                             </Link>
                         )
-                    )
                 })}
                 </div>
             </ModalBody>
